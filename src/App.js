dfbfe15ec66860c2605b90d7277f9c6d14803cd3
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-20 22:58:33 +0100

    a little little

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+import Little from './com/Little.js';
+
 class App extends Component {
 	render(){
-  		return <div>Hello little Component</div>;		
+		return <div><Little/></div>
 	}
 }
 
-render(<App/>, document.body);
+render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,20 @@ var webpack = require('webpack');
 module.exports = {
 
 	entry: './src/App.js',
-	output: {path: __dirname, filename: './bui/bundle.js'}
+	output: {path: __dirname, filename: './bui/bundle.js'},
 
-}
+	module: {
+	    loaders: [
+	      {
+	        test: /.jsx?$/,
+	        loader: 'babel-loader',
+	        exclude: /node_modules/,
+	        query: {
+	          presets: ['es2015', 'react']
+	        }
+	      }
+	    ]
+  	},
+};
 	
 			
