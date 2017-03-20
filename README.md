39647089a3ed16108c0eb9f6f29e21c1f1dfa9c0
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-20 22:04:49 +0100

    hello little element

---
+++ README.md
@@ -0,0 +1,1 @@
+# 40000.react.boilerplate
---
+++ src/App.js
@@ -0,0 +1,5 @@
+var element = document.createElement('div');
+	element.textContent = 'I am your Element';
+	element.style.backgroundColor = 'blue';
+	element.style.color = 'red';
+document.body.appendChild(element);
