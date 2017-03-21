bd27202dd54c58e1fa024bd6ec5dc2625cca978c
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-21 15:12:29 +0100

    structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 var path = require('path');
-var webpack = require('webpack');
 
 module.exports = {
 
-	entry: './src/App.js',
-	output: {path: __dirname, filename: './bui/bundle.js'}
+	entry: './source/App.js',
+	output: {path: __dirname, filename: './built/bundle.js'}
 
 }
 	
