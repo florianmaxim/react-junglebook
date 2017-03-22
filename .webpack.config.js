2d70dbad6412105edb1863adddf510193ff06584
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-22 16:34:36 +0100

    ready for the jungle

--- .webpack.config.js
+++ .webpack.config.js
@@ -18,7 +18,7 @@ module.exports = env => {
 
     output: {
       filename: '[name].[hash].js',
-      path: path.join(__dirname, './build/'),
+      path: path.join(__dirname, './built/'),
     },
 
     module: {
