47fd16e5ed6e22bd669c38409937372ff0f17562
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-23 17:33:32 +0100

    grapes

--- source/index.js
+++ source/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	render(){
-		return <div>You are standing in the jungle with a pineapple in your hand 🍍</div>
+		return <div>You are standing in the jungle with some grapes in your hand 🍇</div>
 	}
 }
 
