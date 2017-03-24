ca1b5fd517855e8d8df376c2f5f7b153ecec18e2
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-24 11:44:44 +0100

    🍎

--- source/index.js
+++ source/index.js
@@ -5,7 +5,7 @@ import Apple from './components/Apple';
 
 class App extends React.Component {
 	render(){
-		return <div>You are standing in the jungle with a <Apple/> in your hand.</div>
+		return <div>You are standing in the jungle with a <Apple/> in your hand. </div>
 	}
 }
 
