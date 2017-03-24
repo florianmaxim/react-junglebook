80be24591e65bf3dc60c1022c7559383c38eade7
Florian Maxim <florianmaxim@Florians-MBP.home>
2017-03-24 12:04:45 +0100

    🍎

--- .webpack.config.js
+++ .webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
 		plugins: [
 			new HtmlWebpackPlugin({
 	      template: path.join(__dirname, './source/index.html'),
-	      filename: 'index.html',
+	      filename: './build/index.html',
 	      inject: 'body',
 	    }),
 		]
